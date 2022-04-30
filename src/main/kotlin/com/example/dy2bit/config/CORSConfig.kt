3844526898277429e4a76bd821d12d236dbed0d3
dev2d0 <leedy2491@ajou.ac.kr>
2022-04-30 20:21:12 +0900

    Fix: cors err release

--- src/main/kotlin/com/example/dy2bit/config/CORSConfig.kt
+++ src/main/kotlin/com/example/dy2bit/config/CORSConfig.kt
@@ -5,13 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
 @Configuration
 class CORSConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-            .maxAge(3600);
+            .maxAge(3600)
     }
 }
--- src/main/kotlin/com/example/dy2bit/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/dy2bit/config/SecurityConfig.kt
@@ -28,6 +28,7 @@ class SecurityConfig(private val jwtUtils: JwtUtils) : WebSecurityConfigurerAdap
 
     override fun configure(http: HttpSecurity) {
         http
+            .cors().and()
             .httpBasic().disable() // rest api만 고려, 기본 설정 해제
             .csrf().disable() // csrf 보안 토큰 disable 처리
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 안함
