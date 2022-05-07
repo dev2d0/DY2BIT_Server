7c05274bd90e7b0f81bd596abc6dc4798bb656a4
dev2d0 <leedy2491@ajou.ac.kr>
2022-05-07 21:44:22 +0900

    Fix: ec2 TimeZone 설정

--- src/main/kotlin/com/example/dy2bit/Dy2bitApplication.kt
+++ src/main/kotlin/com/example/dy2bit/Dy2bitApplication.kt
@@ -3,11 +3,15 @@ package com.example.dy2bit
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableScheduling
 class Dy2bitApplication
 
+@PostConstruct
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     runApplication<Dy2bitApplication>(*args)
 }
